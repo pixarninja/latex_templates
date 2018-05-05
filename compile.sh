2bdc4632c42eb6dea4171c2c53683e4a71fc43fb
pixarninja <wesharris505@gmail.com>
2018-05-05 11:49:20 -0600

    Update compile script to automatically open PDF

--- compile.sh
+++ compile.sh
@@ -3,6 +3,10 @@
 if (($# < 1)); then
 	echo "Usage: ./compile [LaTeX file]"
 else
-	mkdir "./output"
+	mkdir "output"
 	pdflatex --shell-escape --output-directory "./output" $1
+	name=$(echo "$1" | cut -f 1 -d '.')
+	ext=".pdf"
+	path="./output/"$name$ext
+	xdg-open "$path"
 fi
